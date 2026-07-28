c4baee5acf8393543e648fe86b59e2a36ea25914
Praveen230389 <Praveenkumar230389@gmail.com>
2026-07-28 22:44:36 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "priyanka-tfstate-bucket-unique-99"
+    bucket = "backend-tfstate-bucket-praveen"
     key    = "k8/terraform.tfstate"
     region = "us-east-1"
   }
